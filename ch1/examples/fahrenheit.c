819cf3d3574a210954c6e2ba2725aab90196a7e6
Never M <nevermn@protonmail.com>
2019-09-28 19:35:22 +0300

    Add fahrenheit example

---
+++ ch1/examples/fahrenheit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+  int fahr, cel;
+  int lower, upper, step;
+  lower = 0;
+  upper = 300;
+  step = 20;
+  fahr = lower;
+  while (fahr <= upper) {
+    cel = 5 * (fahr-32) / 9;
+    printf("%d\t%d\n", fahr, cel);
+    fahr += step;
+  }
+}
