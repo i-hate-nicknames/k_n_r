beb8de567ea860fd5cbfb74ee80620fe430e121e
Never M <nevermn@protonmail.com>
2019-10-17 11:42:11 +0300

    Add examples

--- ch1/examples/fahrenheit.c
+++ ch1/examples/fahrenheit.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main() {
   float fahr, cel;
-  float lower, upper, step;
-  lower = 0;
-  upper = 300;
-  step = 20;
-  fahr = lower;
+  fahr = LOWER;
   printf("+---------------|--------+\n");
   printf("| Fahrenheit\t| Celsius|\n");
-  while (fahr <= upper) {
+  while (fahr <= UPPER) {
     cel = 5 * (fahr-32) / 9;
     printf("+---------------|--------+\n");
     printf("| %3.1f\t\t| %6.1f |\n", fahr, cel);
-    fahr += step;
+    fahr += STEP;
   }
   printf("+---------------|--------+\n");
 
