ef974c8a5c005f495ee1af628fe1c618120f0c4d
Never M <nevermn@protonmail.com>
2019-11-08 13:44:42 +0200

    Add 1_18 and 1_19

--- ch1/exercises/1_16-19_lines.c
+++ ch1/exercises/1_16-19_lines.c
@@ -2,13 +2,22 @@
 #define MAXLINE 1000
 
 int getln(char copy_to[], int limit);
+int getln_and_trim(char copy_to[], int limit);
 void copy(char from[], char to[]);
 void print_longer_than(int len);
 void print_longest_line();
+void reverse(char s[]);
 
 
 int main() {
-  print_longer_than(80);
+  char str[MAXLINE];
+  int len;
+  while ((len = getln_and_trim(str, MAXLINE)) != -1) {
+    if (len > 0) {
+      puts(str);
+    }
+  }
+  
   return 0;
 }
 
@@ -53,7 +62,6 @@ int getln(char copy_to[], int limit) {
   }
   // null terminate our new string
   copy_to[copied_len] = '\0';
-  //printf(copy_to);
   return len;
 }
 
@@ -66,13 +74,52 @@ void copy(char to[], char from[]) {
 
 // 1_17
 // print all input lines that have length at least len
-void print_longer_than(int len)
-{
+void print_longer_than(int len) {
   char str[MAXLINE];
   int n;
   while ((n = getln(str, MAXLINE)) != 0) {
     if (n >= len) {
       fputs(str, stdout);
     }
+  }
+}
+
+// 1_18
+// get line from stdin, reading at most limit characters
+// put read characters to copy_to
+// return number of characters copied
+// trim all trailing tabs and spaces
+// return 0 if line is empty (only consists of empty spaces)
+// return -1 when the input is exhausted
+int getln_and_trim(char copy_to[], int limit) {
+  char ch;
+  // index of last found non-empty character in the string
+  int last_nonempty = -1;
+  for (int len = 0; (ch = getchar()) != EOF && ch != '\n' && len < limit-1; ++len) {
+    copy_to[len] = ch;
+    if (ch != ' ' && ch != '\t') {
+      last_nonempty = len;
+    }
+  }
+  // we havent found any non-empty characters and the input is over
+  if (last_nonempty == -1 && ch == EOF) {
+    return -1;
+  }
+  // null terminate our new string
+  copy_to[last_nonempty+1] = '\0';
+  return last_nonempty+1;
+}
+
+// 1_19
+void reverse(char s[]) {
+  int len = 0;
+  while (s[len] != '\0') {
+    ++len;
+  }
+  char tmp;
+  for (int i = 0; i < len/2; ++i) {
+    tmp = s[i];
+    s[i] = s[len-i-1];
+    s[len-i-1] = tmp;
   }
 }
