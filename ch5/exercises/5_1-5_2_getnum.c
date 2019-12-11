037669a6f77ef1711efd0237d8d3f3aae529d7a6
Never M <nevermn@protonmail.com>
2019-12-11 09:15:54 +0200

    Implement 5_2 getfloat

--- ch5/exercises/5_1-5_2_getnum.c
+++ ch5/exercises/5_1-5_2_getnum.c
@@ -4,11 +4,17 @@
 int getch(void);
 void ungetch(int);
 int getint(int*);
+int getfloat(double*);
 
 int main(void) {
-  int x, y;
-  x = getint(&y);
-  printf("x: %d, y: %d\n", x, y);
+  int status, x;
+  double t;
+  printf("Enter an integer: \n");
+  status = getint(&x);
+  printf("status: %d, y: %d\n", status, x);
+  printf("Enter a float, values between 0 and 1 should start with 0.: \n");
+  status = getfloat(&t);
+  printf("status: %d, y: %f\n", status, t);
   return 0;
 }
 
@@ -40,6 +46,35 @@ int getint(int* pn) {
   if (c != EOF) {
     ungetch(c);
   }
+  return c;
+}
+
+// 5_2, elegant but can't handle numbers like -.35, .21 etc
+int getfloat(double* pn) {
+  int c, num_part, status, sign;
+  status = getint(&num_part);
+  if (status == EOF) {
+    return status;
+  }
+  sign = (num_part > 0) ? 1 : -1;
+  // assign the whole part
+  *pn = num_part;
+  c = getch();
+  if (c != '.') {
+    ungetch(c);
+    return c;
+  }
+  status = getint(&num_part);
+  if (status <= 0) { // not a number or EOF
+    ungetch('.');
+    return status;
+  }
+  // calculate the actual fractional part given the number after dot
+  double fractional_part = num_part;
+  while (fractional_part > 1) {
+    fractional_part /= 10;
+  }
+  *pn += (sign * fractional_part);
   return c;
 }
 
