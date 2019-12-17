42057166340a477fed710a7ffe54c3ae8941a3a4
Never M <nevermn@protonmail.com>
2019-12-17 10:26:48 +0200

    Implement 5_8

--- ch5/exercises/5_7-5_8_days.c
+++ ch5/exercises/5_7-5_8_days.c
@@ -2,6 +2,9 @@
 
 int day_of_year(int year, int month, int day);
 int month_day(int year, int year_day, int *m, int *d);
+int is_leap(int year);
+int pday_of_year(int year, int month, int day);
+int pmonth_day(int year, int year_day, int *m, int *d);
 
 static char daytab[2][13] = {
  {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
@@ -10,14 +13,17 @@ static char daytab[2][13] = {
 
 int main(void) {
   printf("%d\n", day_of_year(1965, 5, 13));
+  printf("%d\n", pday_of_year(1965, 5, 13));
   int m, d;
   month_day(1965, 133, &m, &d);
+  printf("m: %d, d: %d\n", m, d);
+  pmonth_day(1965, 134, &m, &d);
   printf("m: %d, d: %d\n", m, d);
 }
 
 int day_of_year(int year, int month, int day) {
   int i, leap;
-  leap = (year %4 == 0 && year % 100 != 0) || year %400 == 0;
+  leap = is_leap(year);
   if (month < 1 || month > 12) {
     printf("Error: incorrect month: %d\n", month);
   }
@@ -32,13 +38,42 @@ int day_of_year(int year, int month, int day) {
 
 int month_day(int year, int year_day, int *m, int *d) {
   int i, leap;
-  leap = (year %4 == 0 && year % 100 != 0) || year %400 == 0;
+  leap = is_leap(year);
   if (year_day < 1 || year_day > (365 + leap)) {
     printf("Error: incorrect year: %d\n", year_day);
     return -1;
   }
   for (i = 1; year_day > daytab[leap][i]; i++) {
     year_day -= daytab[leap][i];
+  }
+  *m = i;
+  *d = year_day;
+  return 0;
+}
+
+static char *pdays[] = {daytab[0], daytab[1]};
+
+int is_leap(int year) {
+  return (year %4 == 0 && year % 100 != 0) || year %400 == 0;
+}
+
+int pday_of_year(int year, int month, int day) {
+  int i, leap;
+  leap = is_leap(year);
+  char *days_tab = pdays[leap];
+  for (i = 1; i < month; i++) {
+    day += *(days_tab+i);
+  }
+  return day;
+  
+}
+
+int pmonth_day(int year, int year_day, int *m, int *d) {
+  int i, leap;
+  leap = is_leap(year);
+  char *days_tab = pdays[leap];
+  for (i = 1; year_day > days_tab[i]; i++) {
+    year_day -= days_tab[i];
   }
   *m = i;
   *d = year_day;
