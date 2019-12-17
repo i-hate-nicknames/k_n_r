8066b1e220f7803e0cd30c885f32165fb2429c40
Never M <nevermn@protonmail.com>
2019-12-17 10:09:57 +0200

    Implement error checking for year days

--- ch5/exercises/5_7_days.c
+++ ch5/exercises/5_7_days.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int day_of_year(int year, int month, int day);
-void month_day(int year, int year_day, int *m, int *d);
+int month_day(int year, int year_day, int *m, int *d);
 
 static char daytab[2][13] = {
  {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
@@ -18,18 +18,29 @@ int main(void) {
 int day_of_year(int year, int month, int day) {
   int i, leap;
   leap = (year %4 == 0 && year % 100 != 0) || year %400 == 0;
+  if (month < 1 || month > 12) {
+    printf("Error: incorrect month: %d\n", month);
+  }
+  if (day < 1 || day > daytab[leap][month]) {
+    printf("Error: incorrect day of the month: %d\n", day);
+  }
   for (i = 1; i < month; i++) {
     day += daytab[leap][i];
   }
   return day;
 }
 
-void month_day(int year, int year_day, int *m, int *d) {
+int month_day(int year, int year_day, int *m, int *d) {
   int i, leap;
   leap = (year %4 == 0 && year % 100 != 0) || year %400 == 0;
+  if (year_day < 1 || year_day > (365 + leap)) {
+    printf("Error: incorrect year: %d\n", year_day);
+    return -1;
+  }
   for (i = 1; year_day > daytab[leap][i]; i++) {
     year_day -= daytab[leap][i];
   }
   *m = i;
   *d = year_day;
+  return 0;
 }
