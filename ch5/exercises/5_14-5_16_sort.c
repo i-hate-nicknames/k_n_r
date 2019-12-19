418144039ac1bfde349a284510384d746dbde407
Never M <nevermn@protonmail.com>
2019-12-19 11:05:36 +0200

    Add -f option for case insensitive comparisons

--- ch5/exercises/5_14-5_16_sort.c
+++ ch5/exercises/5_14-5_16_sort.c
@@ -19,18 +19,31 @@ int main(int argc, char **argv) {
   int nlines;
   int numeric = 0;
   int reverse = 0;
+  int case_insensitive = 0;
 
   for (int i = 1; i < argc; i++) {
     if (strcmp(argv[i], "-n") == 0) {
+      if (case_insensitive) {
+        printf("Can't combine options -n and -f\n");
+        return 1;
+      }
       numeric = 1;
     }
     if (strcmp(argv[i], "-r") == 0) {
       reverse = 1;
     }
+    if (strcmp(argv[i], "-f") == 0) {
+      if (numeric) {
+        printf("Can't combine options -n and -f\n");
+        return 1;
+      }
+      case_insensitive = 1;
+    }
   }
   if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
     quicksort((void**) lineptr, 0, nlines-1,
-              (int (*) (void*, void*)) (numeric ? numcmp : strcmp), reverse);
+              (int (*) (void*, void*))
+              (numeric ? numcmp : (case_insensitive ? strcasecmp : strcmp)), reverse);
     writelines(lineptr, nlines);
     return 0;
   } else {
