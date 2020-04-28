50e640de523c7549be330be239a99c57a8eac267
nvm <nevermn@protonmail.com>
2020-04-28 13:41:12 +0300

    Add ifndef to list.h

--- ch6/exercises/list.h
+++ ch6/exercises/list.h
@@ -1,3 +1,6 @@
+#ifndef LIST_H
+#define LIST_H
+
 struct intlist {
   int item;
   struct intlist *tail;
@@ -8,3 +11,5 @@ extern struct intlist *nil;
 struct intlist *cons(int val, struct intlist *onto);
 void delete_list(struct intlist *lst);
 void print_list(struct intlist *lst);
+
+#endif
